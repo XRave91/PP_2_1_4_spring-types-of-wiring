cccb03e70685d38738e40c0764d421b8c7b52a5b
XRave91 <xrave1991@gmail.com>
2022-06-23 16:44:13 +0500

    ready

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-
+    private Egg6 egg6;
+    @Autowired
+    public Duck5(Egg6 egg){egg6=egg;}
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg6.toString();
     }
+
 }
